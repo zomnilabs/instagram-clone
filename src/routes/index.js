492a26a4dce9965e257bacfa6883d0e011361818
Alleo Indong <freedom_war550@live.com.ph>
2019-07-18 22:54:13 +0800

    setup the main page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import MainLayout from "../App";
+
 import Home from "../pages/Home";
 import Users from "../pages/Users";
 import NotFound from "../pages/NotFound";
@@ -7,11 +10,13 @@ import NotFound from "../pages/NotFound";
 function AppRouter() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/users/" component={Users} />
-        <Route path="/*" component={NotFound} />
-      </Switch>
+      <MainLayout>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/users/" component={Users} />
+          <Route path="/*" component={NotFound} />
+        </Switch>
+      </MainLayout>
     </Router>
   );
 }
